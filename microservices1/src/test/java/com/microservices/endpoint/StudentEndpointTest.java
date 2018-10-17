32a7c301842a2a79e1d3ea0c79d72587efccd7da
Rajashri Vishal rane <41443190+RajashriGithub19@users.noreply.github.com>
2018-10-17 16:45:16 +0400

    Update microservices1/src/test/java/com/microservices/endpoint/StudentEndpointTest.java

--- microservices1/src/test/java/com/microservices/endpoint/StudentEndpointTest.java
+++ microservices1/src/test/java/com/microservices/endpoint/StudentEndpointTest.java
@@ -4,41 +4,51 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.microservices.MicroservicesApplication;
 import com.microservices.service.StudentService;
 
+//https://www.baeldung.com/integration-testing-in-spring
+//https://www.baeldung.com/spring-boot-testing
+
 @WebAppConfiguration
 @RunWith(SpringRunner.class)
-//@WebMvcTest(StudentEndpoint.class)
-@SpringBootTest
-@ContextConfiguration(classes= {MicroservicesApplication.class})
-@AutoConfigureMockMvc
+// @WebMvcTest(StudentEndpoint.class)
+// @SpringBootTest
+@ContextConfiguration(classes = { MicroservicesApplication.class })
+// @AutoConfigureMockMvc
 public class StudentEndpointTest {
 
 	@Autowired
+	private WebApplicationContext wac;
+
+	// @Autowired
 	private MockMvc mvc;
-	
-	//@MockBean
+
+	@Before
+	public void setup() throws Exception {
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	// @MockBean
 	@Autowired
 	StudentService studentService;
 
 	@Test
 	public void testEmployeedByID() throws Exception {
 
-		String student = "{\"Firstname\":\"Jack\",\"subjects\":[{\"name\":\"Mathematics\"},{\"name\":\"Computer\"}],\"LastName\":\"Peterson\"}";		
-		
-		mvc.perform(get("/student/11")).andExpect(status().isOk()).andExpect(content().string(student));
+		String student = "{\"Firstname\":\"Jack\",\"subjects\":[{\"name\":\"Mathematics\"},{\"name\":\"Computer\"}],\"LastName\":\"Peterson\"}";
+
+		mvc.perform(get("/student/{studentId}","11")).andExpect(status().isOk()).andExpect(content().string(student));
 	}
 }
